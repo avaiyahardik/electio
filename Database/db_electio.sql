84db45c6a9eaf6ed2f52626e835acb0fa1440850
avaiyahardik <avaiyahardik@gmail.com>
2014-10-20 09:09:24 +0530

    database file changed with new table structure

--- Database/db_electio.sql
+++ Database/db_electio.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2014 at 07:18 AM
+-- Generation Time: Oct 20, 2014 at 05:08 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -29,13 +29,8 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `tbl_candidate` (
   `email` varchar(255) NOT NULL,
   `election_id` int(11) NOT NULL,
-  `firstname` varchar(64) NOT NULL,
-  `lastname` varchar(64) NOT NULL,
-  `criteria` text NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `requirements_file` text NOT NULL,
   `votes` int(11) NOT NULL,
-  `mobile` char(10) NOT NULL,
-  `image` text NOT NULL,
   `manifesto` text NOT NULL,
   PRIMARY KEY (`email`,`election_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -51,9 +46,10 @@ CREATE TABLE IF NOT EXISTS `tbl_election` (
   `election_commissioner_email` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `criteria` text NOT NULL,
+  `requirements` text NOT NULL,
   `type_id` int(11) NOT NULL,
-  `nomination_start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `nomination_start` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `nomination_end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `withdrawal_start` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `withdrawal_end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -74,9 +70,23 @@ CREATE TABLE IF NOT EXISTS `tbl_election_commissioner` (
   `firstname` varchar(64) NOT NULL,
   `lastname` varchar(64) NOT NULL,
   `mobile` char(10) NOT NULL,
-  `organization` text NOT NULL,
+  `organization_id` int(11) NOT NULL,
   `password` varchar(20) NOT NULL,
   PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_election_nominee`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_election_nominee` (
+  `email` varchar(255) NOT NULL,
+  `election_id` int(11) NOT NULL,
+  `requirements_file` text NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`email`,`election_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -103,20 +113,32 @@ INSERT INTO `tbl_election_type` (`type_id`, `type`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_nominator`
+-- Table structure for table `tbl_organization`
 --
 
-CREATE TABLE IF NOT EXISTS `tbl_nominator` (
+CREATE TABLE IF NOT EXISTS `tbl_organization` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `address` text NOT NULL,
+  `about` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user_info`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_user_info` (
   `email` varchar(255) NOT NULL,
-  `election_id` int(11) NOT NULL,
   `firstname` varchar(64) NOT NULL,
   `lastname` varchar(64) NOT NULL,
   `mobile` char(10) NOT NULL,
-  `criteria` text NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `organization_id` int(11) NOT NULL,
   `image` text NOT NULL,
-  `status` tinyint(1) NOT NULL,
-  PRIMARY KEY (`email`,`election_id`)
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
