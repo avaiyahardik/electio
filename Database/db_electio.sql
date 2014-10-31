9149657c9561dcc1045b53ac604d353f2c9efe7e
avaiyahardik <avaiyahardik@gmail.com>
2014-10-31 23:51:02 +0530

    add gender column in tbl_user_info table in database

--- Database/db_electio.sql
+++ Database/db_electio.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2014 at 06:22 AM
+-- Generation Time: Oct 31, 2014 at 07:19 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -42,7 +42,14 @@ CREATE TABLE IF NOT EXISTS `tbl_election` (
   `voting_end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `petition_duration` int(2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_election`
+--
+
+INSERT INTO `tbl_election` (`id`, `election_commissioner_email`, `name`, `description`, `requirements`, `type_id`, `created_at`, `nomination_start`, `nomination_end`, `withdrawal_start`, `withdrawal_end`, `voting_start`, `voting_end`, `petition_duration`) VALUES
+(1, 'avaiyahm@yahoo.com', 'Moviee Chooser', 'Desc', '<p>Req</p>', 2, '2014-10-30 08:01:52', '2014-10-30 09:00:00', '2014-10-30 09:00:00', '2014-10-30 09:00:00', '2014-10-30 09:00:00', '2014-10-30 10:00:00', '2014-10-30 10:30:00', 2);
 
 -- --------------------------------------------------------
 
@@ -58,6 +65,13 @@ CREATE TABLE IF NOT EXISTS `tbl_election_candidate` (
   `manifesto` text,
   PRIMARY KEY (`email`,`election_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_election_candidate`
+--
+
+INSERT INTO `tbl_election_candidate` (`email`, `election_id`, `requirements_file`, `votes`, `manifesto`) VALUES
+('nikita@birmi.com', 1, 'requirements_files\\1414660880004.pdf', 0, 'no manifesto');
 
 -- --------------------------------------------------------
 
@@ -96,6 +110,14 @@ CREATE TABLE IF NOT EXISTS `tbl_election_nominee` (
   PRIMARY KEY (`email`,`election_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tbl_election_nominee`
+--
+
+INSERT INTO `tbl_election_nominee` (`email`, `election_id`, `requirements_file`, `status`) VALUES
+('akashvirani174@gmail.com', 9, 'requirements_files\\1414657575359..pdf', 0),
+('nikita@birmi.com', 1, 'requirements_files\\1414660880004.pdf', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -129,14 +151,16 @@ CREATE TABLE IF NOT EXISTS `tbl_organization` (
   `address` text NOT NULL,
   `about` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Dumping data for table `tbl_organization`
 --
 
 INSERT INTO `tbl_organization` (`id`, `name`, `address`, `about`) VALUES
-(18, 'DA-IICT', 'Gandhinagar', 'Best College');
+(18, 'DA-IICT', 'Gandhinagar', 'Best College'),
+(19, 'DA-IICT', '464, Barpara street', 'Best'),
+(20, 'DA-IICT', 'Gandhinagar', 'Best');
 
 -- --------------------------------------------------------
 
@@ -148,12 +172,21 @@ CREATE TABLE IF NOT EXISTS `tbl_user_info` (
   `email` varchar(255) NOT NULL,
   `firstname` varchar(64) NOT NULL,
   `lastname` varchar(64) NOT NULL,
+  `gender` tinyint(1) NOT NULL,
   `mobile` char(10) NOT NULL,
   `organization_id` int(11) NOT NULL,
   `image` text NOT NULL,
   `password` text NOT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user_info`
+--
+
+INSERT INTO `tbl_user_info` (`email`, `firstname`, `lastname`, `gender`, `mobile`, `organization_id`, `image`, `password`) VALUES
+('akashvirani174@gmail.com', 'Hardik', 'Avaiya', 0, '9197378080', 19, 'user_images\\1414657575317..png', 'a'),
+('nikita@birmi.com', 'Niki', 'Birmi', 0, '9999999999', 20, 'user_images\\1414660880003.png', 'n');
 
 -- --------------------------------------------------------
 
@@ -168,6 +201,13 @@ CREATE TABLE IF NOT EXISTS `tbl_voter` (
   `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`email`,`election_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_voter`
+--
+
+INSERT INTO `tbl_voter` (`email`, `election_id`, `password`, `status`) VALUES
+('sen.daiict@gmail.com', 1, 'XS45M&cg', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
