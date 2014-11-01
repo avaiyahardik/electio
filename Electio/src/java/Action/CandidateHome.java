7074396884bcebf23692cb2201cb6d9996b52eaf
avaiyahardik <avaiyahardik@gmail.com>
2014-11-01 12:51:17 +0530

    code for action=candidate_home to display election and nominee status on home page

--- Electio/src/java/Action/CandidateHome.java
+++ Electio/src/java/Action/CandidateHome.java
@@ -18,41 +18,40 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Vishal Jain
+ * @author Hardik Avaiya
  */
-public class Profile implements Controller.Action {
+public class CandidateHome implements Controller.Action {
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse res) {
 
-        String email = (String) req.getSession().getAttribute("email");
+        String email = (String) req.getSession().getAttribute("candidate_email");
+        String elec_id = (String) req.getSession().getAttribute("election_id");
         String view = "index.jsp";
         String msg = null;
         String err = null;
         String title = "Login";
-        if (email == null || email.equals("")) {
+        if (email == null || email.equals("") || elec_id == null || elec_id.equals("")) {
             err = "Session expired please login again";
         } else {
-            view = "profile.jsp";
-            title = "Profile";
-
+            view = "home.jsp";
+            title = "Nominee/Candidate Home Page";
+            long id = Long.parseLong(elec_id);
             try {
                 DBDAOImplementation obj = DBDAOImplementation.getInstance();
-                ElectionCommissioner ec = obj.getElectionCommissioner(email);
-                Organization org = obj.getOrganization(ec.getOrganization_id());
-                req.setAttribute("election_commissioner", ec);
-                req.setAttribute("organization", org);
+                Election e = obj.getElection(id);
+                int status = obj.getNomineeStatus(id, email);
+                req.setAttribute("election", e);
+                req.setAttribute("nominee_status", status);
             } catch (SQLException ex) {
                 err = ex.getMessage();
-                System.out.println("Profile Err: " + ex.getMessage());
+                System.out.println("Nominee/Candidate Err: " + ex.getMessage());
             }
-
         }
         req.setAttribute("msg", msg);
         req.setAttribute("err", err);
         req.setAttribute("title", title);
         return view;
-
     }
 
 }
