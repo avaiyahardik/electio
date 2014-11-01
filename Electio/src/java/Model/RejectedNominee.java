78e57c7f2a79cf813399741387d5058cdc121d28
avaiyahardik <avaiyahardik@gmail.com>
2014-11-01 16:21:30 +0530

    add RejectedNominee class in model

---
+++ Electio/src/java/Model/RejectedNominee.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Hardik
+ */
+public class RejectedNominee {
+
+    private long election_id;
+    private String email;
+    private String reason;
+
+    public long getElection_id() {
+        return election_id;
+    }
+
+    public void setElection_id(long election_id) {
+        this.election_id = election_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+}
