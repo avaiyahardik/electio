f5cb1b2822fbddaad3a31d1c8a29486acb28311f
avaiyahardik <avaiyahardik@gmail.com>
2014-11-02 13:28:07 +0530

    added new method for password encryption

--- Electio/src/java/Util/RandomString.java
+++ Electio/src/java/Util/RandomString.java
@@ -66,4 +66,27 @@ public class RandomString {
         return encrptedPassword;
     }
 
+    public String encryptPassword2(String password) {
+        String encrypted_password = password;
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(password.getBytes());
+            //Get the hash's bytes 
+            byte[] bytes = md.digest();
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            encrypted_password = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Err : " + e.getMessage());
+        }
+        return encrypted_password;
+    }
+
 }
