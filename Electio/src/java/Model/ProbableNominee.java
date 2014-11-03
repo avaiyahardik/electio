8b12b87b5e6bc11979fd1e16dc9b079d76d12ee4
avaiyahardik <avaiyahardik@gmail.com>
2014-11-03 16:43:52 +0530

    created ProbableNominee class

---
+++ Electio/src/java/Model/ProbableNominee.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Hardik
+ */
+public class ProbableNominee {
+
+    private long election_id;
+    private String email;
+    private boolean status;
+
+    public long getElection_id() {
+        return election_id;
+    }
+
+    public void setElection_id(long election_id) {
+        this.election_id = election_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+}
