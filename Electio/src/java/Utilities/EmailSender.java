7ac281e7406899d8898616ba99e3f6a64f96a6af
mashardarshit <mashar_darshit@yahoo.com>
2014-11-03 17:14:34 +0530

    add sendmail to send one mail

--- Electio/src/java/Utilities/EmailSender.java
+++ Electio/src/java/Utilities/EmailSender.java
@@ -41,9 +41,8 @@ public class EmailSender {
         MimeMessage mimeMessage = new MimeMessage(session);
         try {
             mimeMessage.setFrom(new InternetAddress(from));
-            
+
             mimeMessage.setHeader("Content-Type", "text/html; charset=UTF-8");
-            
 
             InternetAddress[] toAddresses = new InternetAddress[to.length];
 
@@ -53,6 +52,42 @@ public class EmailSender {
             System.out.println("length addresses => " + toAddresses.length);
             mimeMessage.setRecipients(Message.RecipientType.TO, toAddresses);
 
+            mimeMessage.setSubject(subject);
+            mimeMessage.setText(message, "utf-8");
+            mimeMessage.setContent(message, "text/html; charset=utf-8");
+
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, password);
+            transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+            transport.close();
+
+            return true;
+
+        } catch (MessagingException me) {
+            me.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean sendMail(String from, String password, String subject, String message, String to) {
+        String host = "mail.jaintele.com";
+        Properties props = System.getProperties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", password);
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props, null);
+
+        MimeMessage mimeMessage = new MimeMessage(session);
+        try {
+            mimeMessage.setFrom(new InternetAddress(from));
+            mimeMessage.setHeader("Content-Type", "text/html; charset=UTF-8");
+
+            InternetAddress toAddresses = new InternetAddress(to);
+
+            mimeMessage.setRecipient(Message.RecipientType.TO, toAddresses);
             mimeMessage.setSubject(subject);
             mimeMessage.setText(message, "utf-8");
             mimeMessage.setContent(message, "text/html; charset=utf-8");
