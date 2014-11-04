885aeccefa5af45690dc2d1c5db932365d53f50f
avaiyahardik <avaiyahardik@gmail.com>
2014-11-04 23:56:38 +0530

    created electionresult.java action file

---
+++ Electio/src/java/Action/ElectionResult.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Action;
+
+import DAO.DBDAOImplementation;
+import Model.Candidate;
+import Utilities.RandomString;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Hardik
+ */
+public class ElectionResult implements Controller.Action {
+
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse res) {
+
+        String view = "electionResult.jsp";  // default view should be login page itself
+        String title = "Election Result";
+        String msg = null;
+        String err = null;
+        String elec_id = req.getParameter("election_id");
+        if (elec_id == null || elec_id.equals("")) {
+            err = "Election Id is missing";
+            view = "index.jsp";
+            title = "Login";
+        } else {
+            long election_id = Long.parseLong(elec_id);
+
+            try {
+                DBDAOImplementation obj = DBDAOImplementation.getInstance();
+                int election_type = (int) obj.getElectionType(election_id).getType_id();
+                req.setAttribute("election_type", election_type);
+                ArrayList<Candidate> candidates = null;
+                if (election_type == 1) {
+                    candidates = obj.getCandidatesForPreferentialVoting(election_id);
+                } else if (election_type == 2) {
+                    candidates = obj.getCandidatesForWeightedVoting(election_id);
+                }
+                req.setAttribute("candidates", candidates);
+            } catch (SQLException ex) {
+                err = ex.getMessage(); // error message should be displayed on the view page
+                System.out.println("ElectionResult SQL Err: " + ex.getMessage());
+            }
+        }
+        req.setAttribute("msg", msg); // setting msg attribute
+        req.setAttribute("err", err); // setting err attribute
+        req.setAttribute("title", title);
+        return view;
+    }
+}
---
+++ Electio/src/java/Filter/Authenticator.java
@@ -0,0 +1,181 @@
+package Filter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Properties;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Hardik
+ */
+public class Authenticator implements Filter {
+
+    private final String ACTION_MAPPING = "/Controller/ActionMaping.properties";
+    private static final boolean debug = true;
+
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+
+    public Authenticator() {
+
+    }
+
+    private void doBeforeProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+
+    }
+
+    private void doAfterProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+
+    }
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+        doBeforeProcessing(request, response);
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        String action = req.getParameter("action");
+        String view = "index.jsp";
+        String title = "Home";
+        String msg = null;
+        String err = null;
+        Properties map = new Properties();
+        map.load(this.getClass().getClassLoader().getResourceAsStream(ACTION_MAPPING));
+
+        String action_class = null;
+        action_class = map.getProperty(action.toLowerCase());
+        if (action_class == null) {
+            RequestDispatcher rd = req.getRequestDispatcher(view);
+            rd.forward(request, response);
+            return;
+        }
+        chain.doFilter(request, response);
+        doAfterProcessing(request, response);
+    }
+
+    /**
+     * Return the filter configuration object for this filter.
+     */
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+    /**
+     * Set the filter configuration object for this filter.
+     *
+     * @param filterConfig The filter configuration object
+     */
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            if (debug) {
+                log("Authenticator:Initializing filter");
+            }
+        }
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("Authenticator()");
+        }
+        StringBuffer sb = new StringBuffer("Authenticator(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+
+    private void sendProcessingError(Throwable t, ServletResponse response) {
+        String stackTrace = getStackTrace(t);
+
+        if (stackTrace != null && !stackTrace.equals("")) {
+            try {
+                response.setContentType("text/html");
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                PrintWriter pw = new PrintWriter(ps);
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
+
+                // PENDING! Localize this for next official release
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");
+                pw.print(stackTrace);
+                pw.print("</pre></body>\n</html>"); //NOI18N
+                pw.close();
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        } else {
+            try {
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                t.printStackTrace(ps);
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    public static String getStackTrace(Throwable t) {
+        String stackTrace = null;
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+            pw.close();
+            sw.close();
+            stackTrace = sw.getBuffer().toString();
+        } catch (Exception ex) {
+        }
+        return stackTrace;
+    }
+
+    public void log(String msg) {
+        filterConfig.getServletContext().log(msg);
+    }
+
+}
