69011f34d41a40e588d4a3ceedcda574af15c858
avaiyahardik <avaiyahardik@gmail.com>
2014-11-13 01:33:02 +0530

    validate RestartElection

--- Electio/src/java/Action/RestartElection.java
+++ Electio/src/java/Action/RestartElection.java
@@ -7,11 +7,15 @@ package Action;
 
 import DAO.DBDAOImplCandidate;
 import DAO.DBDAOImplElection;
+import DAO.DBDAOImplElectionCommissioner;
+import DAO.DBDAOImplVoter;
+import Model.Election;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
@@ -28,27 +32,45 @@ public class RestartElection implements Controller.Action {
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
         String elec_id = req.getParameter("election_id");
         String email = (String) req.getSession().getAttribute("email");
-        String view = "Controller?action=dashboard";
-        String title = "Dashboard";
+        String view = "index.jsp";
+        String title = "Login";
         String err = null;
         String msg = null;
         if (email == null || email.equals("")) {
             err = "You are not logged in, or session already expired";
         } else {
+            view = "dashboard.jsp";
+            title = "Dashboard";
             try {
-                long election_id = Long.parseLong(elec_id);
-                Timestamp voting_start = new Timestamp(dateFormat.parse(req.getParameter("voting_start")).getTime());
-                Timestamp voting_end = new Timestamp(dateFormat.parse(req.getParameter("voting_end")).getTime());
+                ArrayList<Election> elections = null;
                 DBDAOImplElection objE = DBDAOImplElection.getInstance();
-                DBDAOImplCandidate objC = DBDAOImplCandidate.getInstance();
-                if (objE.updateElectionDates(election_id, voting_start, voting_end) && objC.setVoteZero(election_id)) {
-                    msg = "Election Restarted";
+                elections = objE.getCompletedElections(email);
+                req.setAttribute("elections", elections);
+                if (elec_id == null || elec_id.equals(null)) {
+                    err = "Elecion id missing";
                 } else {
-                    err = "Fail to restart election";
+                    long election_id = Long.parseLong(elec_id);
+                    if (!objE.isValidElectionId(election_id, email)) {
+                        err = "Invalid election id";
+                    } else {
+                        Timestamp voting_start = new Timestamp(dateFormat.parse(req.getParameter("voting_start")).getTime());
+                        Timestamp voting_end = new Timestamp(dateFormat.parse(req.getParameter("voting_end")).getTime());
+                        DBDAOImplCandidate objC = DBDAOImplCandidate.getInstance();
+                        DBDAOImplVoter objV = DBDAOImplVoter.getInstance();
+                        if (objE.updateElectionDates(election_id, voting_start, voting_end) && objC.setVoteZero(election_id) && objV.resetAllVoterStatus()) {
+                            msg = "Election Restarted";
+                        } else {
+                            err = "Fail to restart election";
+                        }
+                    }
                 }
+            } catch (NumberFormatException ex) {
+                err = "Invalid election id";
+                System.out.println("NFE: " + ex);
             } catch (ParseException ex) {
-                Logger.getLogger(RestartElection.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (SQLException ex) {
+                err = "Invalid voting dates";
+                System.out.println("Parse Err: " + ex);
+            } catch (Exception ex) {
                 System.out.println("Restart Election Err: " + ex.getMessage());
             }
         }
