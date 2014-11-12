051e59f0f805d755b70a5e9b532c5bb6d9613a47
avaiyahardik <avaiyahardik@gmail.com>
2014-11-13 00:01:57 +0530

    validate ElectionDetails

--- Electio/src/java/Action/ElectionDetails.java
+++ Electio/src/java/Action/ElectionDetails.java
@@ -3,6 +3,7 @@ package Action;
 import DAO.DBDAOImplElection;
 import Model.Election;
 import java.sql.SQLException;
+import javax.mail.Session;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,31 +17,30 @@ public class ElectionDetails implements Controller.Action {
     public String execute(HttpServletRequest req, HttpServletResponse res) {
 
         String email = (String) req.getSession().getAttribute("voter_email");
-        long election_type = (Long) req.getSession().getAttribute("election_type");
+        String elec_id = (String) req.getSession().getAttribute("election_id");
         String view = "index.jsp";
         String msg = null;
         String err = null;
         String title = "Login";
-        if (email == null || email.equals("")) {
-            err = "Session expired please login again";
+        if (email == null || email.equals("") || elec_id == null || elec_id.equals("")) {
+            err = "Session expired or you are not logged in, please login";
         } else {
-            if (req.getSession().getAttribute("election_id") == null) {
-                err = "Fail to locate election id, please retry";
-            } else {
-                view = "electionDetails.jsp";
-                long election_id = Long.parseLong(req.getSession().getAttribute("election_id").toString());
-                try {
-                    DBDAOImplElection objE = DBDAOImplElection.getInstance();
-                    Election el = objE.getElection(election_id);
-                    req.setAttribute("election", el);
-                } catch (SQLException ex) {
-                    err = ex.getMessage();
-                    System.out.println("VoterLogin SQL Err: " + ex.getMessage());
-                }
+            view = "electionDetails.jsp";
+            try {
+                long election_id = Long.parseLong(elec_id);
+                DBDAOImplElection objE = DBDAOImplElection.getInstance();
+                Election el = objE.getElection(election_id);
+                req.setAttribute("election", el);
+            } catch (NumberFormatException ex) {
+                err = "Invalid organization id";
+                System.out.println("NFE: " + ex);
+            } catch (SQLException ex) {
+                err = ex.getMessage();
+                System.out.println("VoterLogin SQL Err: " + ex.getMessage());
             }
         }
-        req.setAttribute("msg", msg); // setting msg attribute
-        req.setAttribute("err", err); // setting err attribute
+        req.setAttribute("msg", msg);
+        req.setAttribute("err", err);
         req.setAttribute("title", title);
         return view;
     }
