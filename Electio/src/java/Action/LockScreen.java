21511802a5e783aa643c5eb6f64d55cbef516aed
avaiyahardik <avaiyahardik@gmail.com>
2014-11-13 00:25:04 +0530

    validate

--- Electio/src/java/Action/LockScreen.java
+++ Electio/src/java/Action/LockScreen.java
@@ -21,29 +21,23 @@ public class LockScreen implements Controller.Action {
     public String execute(HttpServletRequest req, HttpServletResponse res) {
 
         String email = (String) req.getSession().getAttribute("email");
-        String old_action = req.getParameter("old_action");
         String title = "Screen Locked";
         String msg = null;
         String err = null;
         String view = "lockScreen.jsp";
         String name = null;
-
         try {
-//            DBDAOImplementation obj = DBDAOImplementation.getInstance();
             DBDAOImplElectionCommissioner objEC = DBDAOImplElectionCommissioner.getInstance();
             ElectionCommissioner ec = objEC.getElectionCommissioner(email);
             name = ec.getFirstname() + " " + ec.getLastname();
-
         } catch (SQLException ex) {
             err = ex.getMessage();
             System.out.println("LockScreen Err: " + ex.getMessage());
         }
-        req.setAttribute("old_action", old_action);
         req.setAttribute("name", name);
         req.setAttribute("msg", msg);
         req.setAttribute("err", err);
         req.setAttribute("title", title);
         return view;
     }
-
 }
